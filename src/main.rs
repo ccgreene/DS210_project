de85ec290cac0f87a390d4265754a4e57a4c8646
Charlotte Greene <ccgreene@bu.edu>
2024-12-01 21:22:48 +0000

    commiting the data file
    :wq
    
    commiting the data file

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
 println!("Hello Git?")
+
+//Fn make_directed_graph () -> give directed graph of all developers as nodes
+
+//pagerank
+
+//Fn bfs(maybe a tree) -> diameter, path existence between nodes,  Subtree Sizes and Connectivity, Locating the Closest Common Ancestor (Maybe, seems cool), Distance-Based Connectivity
+
+
+
+//Fn friend_of_friend
+//Fn how_simialr_ are_my_friends_to_My_friends_friends
+    //Overlap between nodes
+//Fn Decide how to measure similarity 
+//Fn find social circles (would these be subtrees with the main tree? What directed graph type should I use) Could I use graph clustering if I only have nodes and edges? Would I be doing that? Is what I’m doing graph clustering? Do I need more data to graph cluster?
+//Fn find social circles
+//Fn compare social circle
+//Fn likelihood of someone in one social circle being connected to someone in a dif social circle
